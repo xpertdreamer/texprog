5756479993678ee5062f9ce8b0da355feadf0560
xpertdreamer <yungnomen@gmail.com>
2026-09-11 22:31:38 +0300

    Start to implemement tokenize method

---
+++ lab_1/counter.cpp
@@ -0,0 +1,13 @@
+#include "counter.h"
+#include <string>
+
+bool Counter::tokenize() {
+  DEBUG("Call tokenize\n");
+  std::size_t i = 0;
+  std::string token;
+  const std::string delimiters = ",.;!?: \t\n\r";
+
+  // TODO: tokenize with complexity O(n)
+
+  return true;
+}
