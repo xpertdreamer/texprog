8fa98622a904b6fb6a8a21a6ae85bf056d1335a7
xpertdreamer <yungnomen@gmail.com>
2026-09-13 01:09:26 +0300

    Optimized version tokenize func(2.4seconds:27MB)

--- lab_1/counter.cpp
+++ lab_1/counter.cpp
@@ -1,13 +1,27 @@
 #include "counter.h"
 #include <string>
 
-bool Counter::tokenize() {
+void Counter::tokenize() {
+  START(tokenize);
   DEBUG("Call tokenize\n");
-  std::size_t i = 0;
+  const std::string delimiters = "\"\',.;!?: \t\n\r";
+  std::string text = buf_stream.str();
   std::string token;
-  const std::string delimiters = ",.;!?: \t\n\r";
-
-  // TODO: tokenize with complexity O(n)
-
-  return true;
+  bool is_del[256] = {false};
+  for (unsigned char d : delimiters) is_del[d] = true;
+  // i dont know if its valid to read char by char but its my maximum for now
+  char c;
+  while (buf_stream.get(c)) {
+      if (is_del[c]) {
+    //if (delimiters.find(c) != std::string::npos) {
+      if (!token.empty()) {
+        tokens.emplace_back(token);
+        token.clear();
+      }
+    } else token += c;
+  }
+  if (!token.empty()) {
+    tokens.emplace_back(token);
+  }
+  END(tokenize);
 }
--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,7 +1,7 @@
 #include "counter.h"
 
 int main(void) {
-    try { Counter counter("lab_1/testcases/test.txt"); }
+    try { Counter counter("lab_1/testcases/war.txt"); counter.tokenize(); counter.print_tokens(); }
     catch(std::exception & e) {
       ERROR("Error occured: %s", e.what());
       return 1;
