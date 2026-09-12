11da9f03fcb3007443444691bd52bec5d45f979d
xpertdreamer <yungnomen@gmail.com>
2026-09-13 01:08:50 +0300

    Time measurment defines

--- lab_1/util.h
+++ lab_1/util.h
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <iostream>
+
 #ifndef _WIN32
 #define ERROR_COLOR "\e[1;31m"
 #define DEBUG_COLOR "\e[1;34m"
@@ -19,3 +22,10 @@
         fprintf(stderr, "%sERROR:%s ", ERROR_COLOR, RESET_COLOR);       \
         fprintf(stderr, fmt, ##__VA_ARGS__);                            \
     } while (0)
+
+#define START(name)                                                            \
+  auto start_##name = std::chrono::high_resolution_clock::now();
+#define END(name) \
+    auto end_##name = std::chrono::high_resolution_clock::now();        \
+    auto duration_##name = std::chrono::duration_cast<std::chrono::microseconds>(end_##name - start_##name).count(); \
+    std::cout << "Timer [" << #name << "] took: " << duration_##name << " microseconds\n";
