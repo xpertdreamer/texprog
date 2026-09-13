9eca46f7061e644818ae476ac91242eee15d510e
xpertdreamer <yungnomen@gmail.com>
2026-09-13 18:17:21 +0300

    Simple CLA parser

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,8 +1,18 @@
 #include "counter.h"
+#include <stdexcept>
 
-int main(void) {
-    try { Counter counter("lab_1/testcases/test.txt"); counter.tokenize(); counter.index(); counter.print_indicies(); }
-    catch(std::exception & e) {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        ERROR("File not providen\n");
+        return 1;
+    }
+    try {
+        const std::string path = *(argv+1);
+        Counter counter(path);
+        counter.tokenize(); counter.index();
+        counter.print_indicies();
+    }
+    catch(std::invalid_argument & e) {
       ERROR("Error occured: %s", e.what());
       return 1;
     }
