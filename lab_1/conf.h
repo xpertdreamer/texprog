ab0ef8a406e660109bdc01230171d289044d099c
xpertdreamer <yungnomen@gmail.com>
2026-09-13 22:05:35 +0300

    Conf file docs

--- lab_1/conf.h
+++ lab_1/conf.h
@@ -1,2 +1,12 @@
+/**
+ * @file conf.h
+ * @brief Compile-time configuration flags for the project.
+ */
+
+#ifndef CONF_H
+#define CONF_H
+
 #define DEBUG_MODE 1  ///<Use 1 to enable debug lines in output
 #define TEST 0        ///<Use 1 to print file content after read
+
+#endif
