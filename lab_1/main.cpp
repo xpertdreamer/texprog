a1be5bc5db034c3e3043a885edbadceb24ea0445
xpertdreamer <yungnomen@gmail.com>
2026-09-13 17:38:52 +0300

    count and print counted in main

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,7 +1,7 @@
 #include "counter.h"
 
 int main(void) {
-    try { Counter counter("lab_1/testcases/war.txt"); counter.tokenize(); counter.print_tokens(); }
+    try { Counter counter("lab_1/testcases/war.txt"); counter.tokenize(); counter.count(); counter.print_counted(); }
     catch(std::exception & e) {
       ERROR("Error occured: %s", e.what());
       return 1;
