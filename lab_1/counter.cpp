30ae5b30f9df910e43236db824b2211b66d01f6f
xpertdreamer <yungnomen@gmail.com>
2026-09-13 17:39:27 +0300

    Correct way to lower russian letters

--- lab_1/counter.cpp
+++ lab_1/counter.cpp
@@ -1,9 +1,33 @@
 #include "counter.h"
 #include <string>
 
-static bool is_capitalized() {
-    // TODO: check if first character is capital and replace with small one
-    return true;
+static void lower(std::string& token) {
+    if (token.empty()) return;
+    unsigned char c1 = static_cast<unsigned char>(token[0]);
+    // check ASCII code of first char
+    if (c1 >= 'A' && c1 <= 'Z') {
+        token[0] = static_cast<char>(c1 + 32);
+        return;
+    }
+    // ru letter has 2 bytes
+    if (token.size() >= 2) {
+        unsigned char c2 = static_cast<unsigned char>(token[1]);
+        // Ё = D0 81; ё = D1 91
+        if (c1 == 0xD0 && c2 == 0x81) {
+            token[0] = static_cast<char>(0xD1);
+            token[1] = static_cast<char>(0x91);
+            return;
+        }
+        // A-Я = D0 90 - D0 AF
+        if (c1 == 0xD0 && c2 >= 0x90 && c2 <= 0xAF) {
+            if (c2 <= 0x9F) {
+                token[1] = static_cast<char>(c2 + 0x20);
+            } else {
+                token[0] = static_cast<char>(0xD1);
+                token[1] = static_cast<char>(c2 - 0x20);
+            }
+        }
+    }
 }
 
 void Counter::tokenize() {
