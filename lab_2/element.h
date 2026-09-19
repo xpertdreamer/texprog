1016b7f42863a60f0e70c71c515a7593234aa56d
xpertdreamer <yungnomen@gmail.com>
2026-09-19 14:51:01 +0300

    Almost all base elements

---
+++ lab_2/element.h
@@ -0,0 +1,37 @@
+#include "util.h"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class Element {
+    public:
+        virtual ~Element() = default;
+};
+
+class Header : public Element {
+    private:
+        uint8_t level = 1;
+        std::string text = "";
+    public:
+        inline Header(uint8_t l, std::string& t) : level(l), text(std::move(t)) { DEBUG("Header initialized\tlevel=%u\ttext=%s", l, t.c_str()); }
+};
+
+class Paragraph : public Element {
+    private:
+        std::string text = "";
+    public:
+        inline Paragraph(std::string& t) : text(std::move(t)) { DEBUG("Paragraph initialized\ttext=%s", text.c_str()); }
+};
+
+struct ListElement {
+    std::string text = "";
+    uint8_t level = 1;
+};
+
+class List : public Element {
+    private:
+        std::vector<ListElement> elements;
+        bool ordered = false;
+    public:
+};
