de7850dcbf77453fd05f37a520189c416dc6a368
xpertdreamer <yungnomen@gmail.com>
2026-09-19 23:45:48 +0300

    Naming refactoring & documentation

--- lab_2/parser.h
+++ lab_2/parser.h
@@ -4,8 +4,94 @@
 #include <string>
 #include <regex>
 
-#define HTML_SIGNS      R"(<\s*\/?\s*(html|body|div|p|span|a)\s*>)"
-#define HEADER_MD_SIGNS R"(^(#{1,6})\s+(.+)$)"
+/**
+ * @def HTML_HEADER_SIGNS
+ * @brief Regular expression for detecting HTML header tags.
+ * Matches opening and closing header tags from h1 to h6.
+ * @details Regex breakdown:
+ *
+ * - '\s*' - zero or more whitspaces
+ * - '<h[1-6]' - literal <h followed by 1-6
+ * - '[^>]*' - zero or more characters (not >)
+ * - '>' - closing bracket literal
+ * - '|' - OR
+ * - '<\/h[1-6]>' - literal closing tag
+ */
+#define HTML_HEADER_SIGNS    R"(\s*<h[1-6][^>]*>|<\/h[1-6]>)"
+
+/**
+ * @def HTML_PARAGRAPH_SIGNS
+ * @brief Regular expression for detecting HTML paragraph tags.
+ * Matches opening and closing 'p' tags.
+ * @details Regex breakdown:
+ *
+ * - '\s*' - zero or more whitspaces
+ * - '<p' - literal itself
+ * - '[^>]*' - zero or more characters (not >)
+ * - '>' - closing bracket literal
+ * - '|' - OR
+ * - '<\/p>' - literal closing tag
+ */
+#define HTML_PARAGRAPH_SIGNS R"(\s*<p[^>]*>|<\/p>)"
+
+/**
+ * @def HTML_LIST_SIGNS
+ * @brief Regular expression for detecting HTML list tags.
+ * Matches opening and closing tags of unordered and ordered lists.
+ * @details Regex breakdown:
+ *
+ * - '\s*' - zero or more whitspaces
+ * - '<' - literal itself
+ * - '(ul|ol|li)' - literal group (tag itself)
+ * - '[^>]*' - zero or more characters (not >)
+ * - '>' - closing bracket literal
+ * - '|' - OR
+ * - '<\/(il|ol|li)>' - literal closing tag
+ */
+#define HTML_LIST_SIGNS      R"(\s*<(ul|ol|li)[^>]*>|<\/(ul|ol|li)>)"
+
+/**
+ * @def MD_HEADER_SIGNS
+ * @brief Regular expression for detecting Markdown headers.
+ * Matches Markdown headers with levels 1-6
+ * @details Regex breakdown:
+ *
+ * - '^' - start of the line
+ * - '(#{1,6})' - literal '#' from 1 to 6 times in a row
+ * - '\s+' - one or more whitespaces
+ * - '(.+)' - any header text (at least one character)
+ * - '$' - end of the line
+ */
+#define MD_HEADER_SIGNS      R"(^(#{1,6})\s+(.+)$)"
+
+/**
+ * @def MD_UNORDERED_SIGNS
+ * @brief Regular expression for detecting Markdown unordered lists.
+ * Matches Markdown lines starting with '-', '+' or '*'.
+ * @details Regex breakdown:
+ *
+ * - '^' - start of the line
+ * - '\s*' - zero or more whitspaces
+ * - '[-+*]' - any bullet marker
+ * - '(.+)' - any text (at least one character)
+ * - '$' - end of the line
+ */
+#define MD_UNORDERED_SIGNS   R"(^\s*[-+*]\s+(.+)$)"
+
+/**
+ * @def MD_ORDERED_SIGNS
+ * @brief Regular expression for detecting Markdown ordered lists.
+ * Matches Markdown lines starting with number following by dot and whitespace
+ * @details Regex breakdown:
+ *
+ * - '^' - start of the line
+ * - '\s*' - zero or more whitspaces
+ * - '\d+' - one or more digits
+ * - '\.' - literal '.'
+ * - '(.+)' - any text (at least one character)
+ * - '$' - end of the line
+ */
+#define MD_ORDERED_SIGNS     R"(^\s*\d+\.\s+(.+)$)"
 
 class Parser {
     static bool
